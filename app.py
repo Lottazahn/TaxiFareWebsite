220f412e183a6778166c17018484151ffada4399
Lotta Zahn <130829990+Lottazahn@users.noreply.github.com>
2023-05-15 17:50:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,34 +6,21 @@ import requests
 '''
 
 # What do we see?
-ingredient = st.text_input("Which ingredient would you like to use?", value="vodka")
+meal = st.text_input("Which meal do you eat?", value="pizza margharita")
 
 # Button
 if st.button('Search'):
-    url = "https://the-cocktail-db.p.rapidapi.com/search.php"
-
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByNutrients"
+    
     # API
     # Enter here the address of your Flask API
-    querystring = {"s": f"{ingredient}"}
+    querystring = {"limitLicense":"false","minProtein":"0","minVitaminC":"0","minSelenium":"0","maxFluoride":"50","maxVitaminB5":"50","maxVitaminB3":"50","maxIodine":"50","minCarbs":"0","maxCalories":"250","minAlcohol":"0","maxCopper":"50","maxCholine":"50","maxVitaminB6":"50","minIron":"0","maxManganese":"50","minSodium":"0","minSugar":"0","maxFat":"20","minCholine":"0","maxVitaminC":"50","maxVitaminB2":"50","minVitaminB12":"0","maxFolicAcid":"50","minZinc":"0","offset":"0","maxProtein":"100","minCalories":"0","minCaffeine":"0","minVitaminD":"0","maxVitaminE":"50","minVitaminB2":"0","minFiber":"0","minFolate":"0","minManganese":"0","maxPotassium":"50","maxSugar":"50","maxCaffeine":"50","maxCholesterol":"50","maxSaturatedFat":"50","minVitaminB3":"0","maxFiber":"50","maxPhosphorus":"50","minPotassium":"0","maxSelenium":"50","maxCarbs":"100","minCalcium":"0","minCholesterol":"0","minFluoride":"0","maxVitaminD":"50","maxVitaminB12":"50","minIodine":"0","maxZinc":"50","minSaturatedFat":"0","minVitaminB1":"0","maxFolate":"50","minFolicAcid":"0","maxMagnesium":"50","minVitaminK":"0","maxSodium":"50","maxAlcohol":"50","maxCalcium":"50","maxVitaminA":"50","maxVitaminK":"50","minVitaminB5":"0","maxIron":"50","minCopper":"0","maxVitaminB1":"50","number":"10","minVitaminA":"0","minPhosphorus":"0","minVitaminB6":"0","minFat":"5","minVitaminE":"0"}
 
-    headers = {
-        "X-RapidAPI-Key": "a1cbd55fa4msh4fe6d0f423ccb9ep1066a8jsna6b685db9cc1",
-        "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com"
-    }
+    headers = {"X-RapidAPI-Key": "a1cbd55fa4msh4fe6d0f423ccb9ep1066a8jsna6b685db9cc1",
+	"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"}
+        
+   response = requests.get(url, headers=headers, params=querystring)
 
-    response = requests.get(url, headers=headers, params=querystring)
-    data = response.json()
+print(response.json())
 
-    # Display cocktail details
-    if 'drinks' in data:
-        for drink in data['drinks']:
-            drink_name = drink['strDrink']
-            drink_image = drink['strDrinkThumb']
-
-            # Display cocktail name
-            st.write(f"Cocktail Name: {drink_name}")
-
-            # Display cocktail image
-            st.image(drink_image, caption=drink_name, use_column_width=True)
-    else:
-        st.write("No cocktails found for the given ingredient.")
+    
