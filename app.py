58e06a71291bc8d8dd50f73d242e0aaeb4cf3eff
Lotta Zahn <130829990+Lottazahn@users.noreply.github.com>
2023-05-15 15:28:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,38 +6,27 @@ import datetime
 import requests
 
 '''
-# TaxiFareModel front
-
-This front queries the Le Wagon [taxi fare model API](https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2)
-'''
-
-pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-pickup_datetime = f'{pickup_date} {pickup_time}'
-pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
-pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
-dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
-dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
-passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
-
-if st.button('Sumbit'):
+# Recipe Finder
 
 
-    # enter here the address of your flask api
-    url = 'https://taxifare.lewagon.ai/predict'
+-- what do we see?
+ingredient = st.text_input ("Which ingredient would you like to use?",value="vodka")
 
-    params = dict(
-        pickup_datetime=pickup_datetime,
-        pickup_longitude=pickup_longitude,
-        pickup_latitude=pickup_latitude,
-        dropoff_longitude=dropoff_longitude,
-        dropoff_latitude=dropoff_latitude,
-        passenger_count=passenger_count)
+-- button
+if st.button('Search'):
 
-    response = requests.get(url, params=params)
+-- API
+# enter here the address of your flask api
 
-    prediction = response.json()
+querystring = {"s":"vodka"}
 
-    pred = prediction['fare']
+headers = {
+	"X-RapidAPI-Key": "a1cbd55fa4msh4fe6d0f423ccb9ep1066a8jsna6b685db9cc1",
+	"X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com"
+}
 
-    pred
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
+
+
